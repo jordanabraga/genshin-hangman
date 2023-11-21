3b63d3b088016c287bda561867f0243b01b724d8
jordana braga <danaxbraga@gmail.com>
2023-11-21 02:21:29 +0000

    Add function to add typing effect on text

--- run.py
+++ run.py
@@ -3,3 +3,10 @@ import sys
 import time
 import simple_colors
 
+# Function to display text as if typed
+
+def sprint(str):
+    for c in str + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(6./90)
